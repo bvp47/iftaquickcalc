8e1f25b877264387703e0fa0c5fc5ffd5b3753bc
bvp47 <143102768+bvp47@users.noreply.github.com>
2025-07-31 12:01:04 -0500

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,6 +1,6 @@
+// lib/supabaseServer.ts
 import { cookies } from 'next/headers'
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 
-export const createServerClient = () => {
-  return createRouteHandlerClient({ cookies })
-}
+export const createServerClient = () =>
+  createRouteHandlerClient({ cookies })
