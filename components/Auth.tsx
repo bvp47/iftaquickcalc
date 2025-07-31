5888c7bf95e932660b4027be3fe105f977e52319
bvp47 <143102768+bvp47@users.noreply.github.com>
2025-07-31 12:28:55 -0500

    Update Auth.tsx

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react'
-import { createClient } from '@/lib/supabase'
+import { createClient } from '@/lib/supabaseClient'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
