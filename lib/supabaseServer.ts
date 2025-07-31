be8db5e7c92ccffe2219aa05b412cd24c32bc214
bvp47 <143102768+bvp47@users.noreply.github.com>
2025-07-31 13:45:07 -0500

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,6 +1,11 @@
 import { cookies } from 'next/headers'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 
+export const supabaseServer = () => {
+  return createServerComponentClient({ cookies })
+}
+
+// Add this export to match the import in create-checkout.ts
 export const createServerSupabaseClient = () => {
   return createServerComponentClient({ cookies })
 }
